918af6e9f92c4ddb3b4dca1a9b92033209ae6d15
Maël DONNART <mael.donnart1@gmail.com>
2024-12-02 11:57:43 +0100

    feat: Day 2

---
+++ internal/day2/day2.go
@@ -0,0 +1,57 @@
+package day2
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func PartOne(input []string) (int, error) {
+	var safeReports int
+	for _, line := range input {
+		numbers := strings.Split(line, " ")
+		if isSafe(numbers) {
+			safeReports++
+		}
+	}
+
+	return safeReports, nil
+}
+
+func PartTwo(input []string) (int, error) {
+	var safeReports int
+	for _, line := range input {
+		numbers := strings.Split(line, " ")
+		for i := 0; i < len(numbers); i++ {
+			s := slices.Delete(slices.Clone(numbers), i, i+1)
+			if isSafe(s) {
+				safeReports++
+				break
+			}
+		}
+	}
+
+	return safeReports, nil
+}
+
+func isSafe(numbers []string) bool {
+	var (
+		isIncreasing = true
+		isDecreasing = true
+	)
+
+	for i := 1; i < len(numbers); i++ {
+		curr, _ := strconv.Atoi(numbers[i])
+		prev, _ := strconv.Atoi(numbers[i-1])
+
+		diff := curr - prev
+		if diff <= 0 || diff > 3 {
+			isIncreasing = false
+		}
+		if diff >= 0 || -diff > 3 {
+			isDecreasing = false
+		}
+	}
+
+	return isIncreasing || isDecreasing
+}
