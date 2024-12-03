6b9d3edadcf7a44c780f2131b4ce494ce92232dd
Maël DONNART <mael.donnart1@gmail.com>
2024-12-03 08:28:34 +0100

    feat: Day 3

---
+++ internal/day3/day3.go
@@ -0,0 +1,44 @@
+package day3
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func PartOne(input []string) (int, error) {
+	text := strings.Join(input, "")
+
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	matches := re.FindAllStringSubmatch(text, -1)
+
+	var sum int
+	for _, match := range matches {
+		x, _ := strconv.Atoi(match[1])
+		y, _ := strconv.Atoi(match[2])
+		sum += x * y
+	}
+
+	return sum, nil
+}
+
+func PartTwo(input []string) (int, error) {
+	text := strings.Join(input, "")
+
+	re := regexp.MustCompile(`don't\(\).*?do\(\)`)
+	text = re.ReplaceAllString(text, "")
+
+	text = strings.Split(text, "don't()")[0]
+
+	re = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	matches := re.FindAllStringSubmatch(text, -1)
+
+	var sum int
+	for _, match := range matches {
+		x, _ := strconv.Atoi(match[1])
+		y, _ := strconv.Atoi(match[2])
+		sum += x * y
+	}
+
+	return sum, nil
+}
