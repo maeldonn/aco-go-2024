8dbd395030a04a4c74ed89cfa06cc05c23feba2f
Maël DONNART <mael.donnart1@gmail.com>
2024-12-07 01:40:56 +0100

    feat: Day 7

---
+++ internal/day7/day7.go
@@ -0,0 +1,96 @@
+package day7
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func PartOne(input []string) (int, error) {
+	var totalCalibration int
+	for _, line := range input {
+		split := strings.Split(line, " ")
+
+		var (
+			testValue, _ = strconv.Atoi(split[0][:len(split[0])-1])
+			nums         = split[1:]
+		)
+
+		var checkCombo func(slice []string, length int) bool
+		checkCombo = func(combo []string, length int) bool {
+			if length == len(nums)-1 {
+				if eval(nums, combo) == testValue {
+					totalCalibration += testValue
+					return true
+				}
+				return false
+			}
+
+			for _, operator := range []string{"*", "+"} {
+				if checkCombo(append(combo, operator), length+1) {
+					return true
+				}
+			}
+
+			return false
+		}
+
+		checkCombo([]string{}, 0)
+	}
+
+	return totalCalibration, nil
+}
+
+func PartTwo(input []string) (int, error) {
+	var totalCalibration int
+	for _, line := range input {
+		split := strings.Split(line, " ")
+
+		var (
+			testValue, _ = strconv.Atoi(split[0][:len(split[0])-1])
+			nums         = split[1:]
+		)
+
+		var checkCombo func(slice []string, length int) bool
+		checkCombo = func(combo []string, length int) bool {
+			if length == len(nums)-1 {
+				if eval(nums, combo) == testValue {
+					totalCalibration += testValue
+					return true
+				}
+				return false
+			}
+
+			for _, operator := range []string{"*", "+", "||"} {
+				if checkCombo(append(combo, operator), length+1) {
+					return true
+				}
+			}
+
+			return false
+		}
+
+		checkCombo([]string{}, 0)
+	}
+
+	return totalCalibration, nil
+}
+
+func eval(nums, combo []string) int {
+	total, _ := strconv.Atoi(nums[0])
+
+	for i := 1; i < len(nums); i++ {
+		n, _ := strconv.Atoi(nums[i])
+		switch combo[i-1] {
+		case "*":
+			total *= n
+		case "+":
+			total += n
+		case "||":
+			conc, _ := strconv.Atoi(fmt.Sprintf("%d%d", total, n))
+			total = conc
+		}
+	}
+
+	return total
+}
---
+++ internal/day7/day7_test.go
@@ -0,0 +1,31 @@
+package day7
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var input = []string{
+	"190: 10 19",
+	"3267: 81 40 27",
+	"83: 17 5",
+	"156: 15 6",
+	"7290: 6 8 6 15",
+	"161011: 16 10 13",
+	"192: 17 8 14",
+	"21037: 9 7 18 13",
+	"292: 11 6 16 20",
+}
+
+func TestPartOne(t *testing.T) {
+	got, err := PartOne(input)
+	assert.NoError(t, err)
+	assert.Equal(t, 3749, got)
+}
+
+func TestPartTwo(t *testing.T) {
+	got, err := PartTwo(input)
+	assert.NoError(t, err)
+	assert.Equal(t, 11387, got)
+}
