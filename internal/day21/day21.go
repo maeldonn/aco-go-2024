8b0338ad7ca5c61a7684f4f159e05efdc70d56ea
Maël DONNART <mael.donnart1@gmail.com>
2024-12-21 14:16:33 +0100

    feat: Day 21

---
+++ internal/day21/day21.go
@@ -0,0 +1,9 @@
+package day21
+
+func PartOne(input []string) any {
+	return 0
+}
+
+func PartTwo(input []string) any {
+	return 0
+}
---
+++ internal/day21/day21_test.go
@@ -0,0 +1,21 @@
+package day21
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var input = []string{
+	"",
+}
+
+func TestPartOne(t *testing.T) {
+	got := PartOne(input)
+	assert.Equal(t, 0, got)
+}
+
+func TestPartTwo(t *testing.T) {
+	got := PartTwo(input)
+	assert.Equal(t, 0, got)
+}
